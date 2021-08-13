98a44c061b41ce642fcb4e797bf0b2638030ce9c
Sethi77 <devanshsethi77@gmail.com>
2021-08-13 19:25:09 +0530

    Project FoodOn

---
+++ app/src/main/java/my/foodon/app/Chef.java
@@ -0,0 +1,14 @@
+package my.foodon.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chef extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef);
+    }
+}
